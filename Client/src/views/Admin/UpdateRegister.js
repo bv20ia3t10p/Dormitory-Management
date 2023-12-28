46b374b03353e8729daeeaa8a73f8763d8c59958
Phạm Trọng Phúc <94206242+bv20ia3t10p@users.noreply.github.com>
2023-12-28 20:36:14 +0700

    Update UpdateRegister.js

--- Client/src/views/Admin/UpdateRegister.js
+++ Client/src/views/Admin/UpdateRegister.js
@@ -1,146 +1,196 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from "react";
 import {
-    Form, Row, FormGroup, Col, Input, Button, Modal, ModalHeader, ModalBody, ModalFooter, Label
-} from 'reactstrap';
+  Form,
+  Row,
+  FormGroup,
+  Col,
+  Input,
+  Button,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  Label,
+} from "reactstrap";
+import { getRooms } from "./AddReceipt";
+import {
+  Autocomplete,
+  Select,
+  TextField,
+  FormControl,
+  MenuItem,
+} from "@mui/material";
 
 function UpdateRegister(props) {
-    let RegisterRoom = props.currentObject;
-    console.log("check prop registerroom: ", RegisterRoom)
-
-    const [state, setState] = useState({
-        id: RegisterRoom.ObjectEdit.id,
-        studentId: RegisterRoom.ObjectEdit.studentId,
-        roomId: RegisterRoom.ObjectEdit.roomId,
-        dateBegin: RegisterRoom.ObjectEdit.dateBegin,
-        numberOfMonth: RegisterRoom.ObjectEdit.numberOfMonth,
-        domitoryFeeStatus: RegisterRoom.ObjectEdit.domitoryFeeStatus,
-        status: RegisterRoom.ObjectEdit.status,
+  let RegisterRoom = props.currentObject;
+  console.log("check prop registerroom: ", RegisterRoom);
+  const [rooms, setRooms] = useState([]);
+  useEffect(() => {
+    getRooms(setRooms);
+  }, []);
+  const [selectedRoom, setSelectedRoom] = useState();
+  const [state, setState] = useState({
+    id: RegisterRoom.ObjectEdit.id,
+    studentId: RegisterRoom.ObjectEdit.studentId,
+    roomId: RegisterRoom.ObjectEdit.roomId,
+    dateBegin: RegisterRoom.ObjectEdit.dateBegin,
+    numberOfMonth: RegisterRoom.ObjectEdit.numberOfMonth,
+    domitoryFeeStatus: RegisterRoom.ObjectEdit.domitoryFeeStatus,
+    status: RegisterRoom.ObjectEdit.status,
+  });
+  console.log("check state in children: ", state);
+  const handleOnchangeInput = (event, item) => {
+    let copyState = { ...state };
+    console.log("check event: ", event);
+    if (event.target.value == "true" && item == "domitoryFeeStatus") {
+      copyState["domitoryFeeStatus"] = true;
+    } else if (event.target.value == "false" && item == "domitoryFeeStatus") {
+      copyState["domitoryFeeStatus"] = false;
+    } else if (event.target.value == "true" && item == "status") {
+      copyState["status"] = true;
+    } else if (event.target.value == "false" && item == "status") {
+      copyState["status"] = false;
+    } else {
+      copyState[item] = event.target.value;
+    }
+    setState({
+      ...copyState,
     });
-    console.log("check state in children: ", state);
-    const handleOnchangeInput = (event, item) => {
-        let copyState = { ...state }
-        console.log('check event: ', event);
-        if (event.target.value == "true" && item == "domitoryFeeStatus") {
-            copyState["domitoryFeeStatus"] = true;
-        }
-        else if (event.target.value == "false" && item == "domitoryFeeStatus") {
-            copyState["domitoryFeeStatus"] = false;
-        }
-        else if (event.target.value == "true" && item == "status") {
-            copyState["status"] = true;
-        }
-        else if (event.target.value == "false" && item == "status") {
-            copyState["status"] = false;
-        }
-        else {
-            copyState[item] = event.target.value;
-        }
-        setState({
-            ...copyState
-        })
+  };
+  const checkValideInput = () => {
+    let isValid = true;
+    let arrInput = ["studentId", "roomId", "dateBegin", "numberOfMonth"];
+    for (let i = 0; i < arrInput.length; i++) {
+      console.log("check inside loop", state[arrInput[i]], arrInput[i]);
+      if (!state[arrInput[i]]) {
+        isValid = false;
+        alert("Missing parameter: " + arrInput[i]);
+        break;
+      }
     }
-    const checkValideInput = () => {
-        let isValid = true;
-        let arrInput = ['studentId', 'roomId', 'dateBegin', 'numberOfMonth']
-        for (let i = 0; i < arrInput.length; i++) {
-            console.log('check inside loop', state[arrInput[i]], arrInput[i])
-            if (!state[arrInput[i]]) {
-                isValid = false;
-                alert('Missing parameter: ' + arrInput[i]);
-                break;
-            }
-        }
-        return isValid;
+    return isValid;
+  };
+  const handleUpdateRegister = () => {
+    let isValid = checkValideInput();
+    if (isValid === true) {
+      props.updateObject(state);
+      console.log("check data modalUpdate: ", state);
     }
-    const handleUpdateRegister = () => {
-        let isValid = checkValideInput();
-        if (isValid === true) {
-            props.updateObject(state);
-            console.log("check data modalUpdate: ", state);
-        }
-    }
-    return (
-        <div>
-            <Modal isOpen={props.modal} fade={false} toggle={props.toggle}>
-                <ModalHeader >Update RegisterRoom</ModalHeader>
-                <ModalBody>
-                    <Form >
-                        <FormGroup>
-                            <Label for="studentId">
-                                MSSV
-                            </Label>
-                            <Input
-                                id="studentId"
-                                name="studentId"
-                                onChange={(event) => handleOnchangeInput(event, "studentId")}
-                                value={state.studentId}
-                            />
-                        </FormGroup>
-                        <FormGroup>
-                            <Label for="roomId">
-                                Mã phòng
-                            </Label>
-                            <Input
-                                id="roomId"
-                                name="roomId"
-                                onChange={(event) => handleOnchangeInput(event, "roomId")}
-                                value={state.roomId}
-                            />
-                        </FormGroup>
-                        <FormGroup>
-                            <Label for="dateBegin">
-                                Ngày bắt đầu ở
-                            </Label>
-                            <Input
-                                id="dateBegin"
-                                name="dateBegin"
-                                type="date"
-                                onChange={(event) => handleOnchangeInput(event, "dateBegin")}
-                                value={state.dateBegin}
-                            />
-                        </FormGroup>
-                        <FormGroup>
-                            <Label for="numberOfMonth">
-                                Số tháng dự kiến lưu trú
-                            </Label>
-                            <Input
-                                id="numberOfMonth"
-                                name="numberOfMonth"
-                                onChange={(event) => handleOnchangeInput(event, "numberOfMonth")}
-                                value={state.numberOfMonth}
-                            />
-                        </FormGroup>
-                        <Label for="gender">
-                            Tình trạng hóa đơn
-                        </Label>
-
-                        <FormGroup>
-                            <select onChange={(event) => handleOnchangeInput(event, "domitoryFeeStatus")}>
-                                <option >--Chọn tình trạng hóa đơn--</option>
-                                <option value={true}>Đã thanh toán</option>
-                                <option value={false}>Chưa thanh toán</option>
-                            </select>
-                        </FormGroup>
-                        <Label for="status">
-                            Trạng thái
-                        </Label>
-                        <FormGroup>
-                            <select onChange={(event) => handleOnchangeInput(event, "status")}>
-                                <option>-- Chọn trạng thái--</option>
-                                <option value={true}>Còn hạn</option>
-                                <option value={false}>Hết hạn</option>
-                            </select>
-                        </FormGroup>
-                        <Button color="primary" onClick={() => handleUpdateRegister()}>
-                            Cập nhật
-                        </Button>{' '}
-                        <Button color="secondary" onClick={props.toggle}>
-                            Quay lại
-                        </Button>
-                    </Form>
-                </ModalBody>
-                {/* Nút tắt phần modal */}
-                {/* <ModalFooter>
+  };
+  return (
+    <div>
+      <Modal isOpen={props.modal} fade={false} toggle={props.toggle}>
+        <ModalHeader>Update RegisterRoom</ModalHeader>
+        <ModalBody>
+          <Form>
+            <FormGroup>
+              <Label for="studentId">MSSV</Label>
+              <Input
+                disabled
+                id="studentId"
+                name="studentId"
+                onChange={(event) => handleOnchangeInput(event, "studentId")}
+                value={state.studentId}
+              />
+            </FormGroup>
+            {rooms && (
+              <Autocomplete
+                options={rooms}
+                getOptionLabel={(r) => r.name}
+                value={selectedRoom}
+                id="roomId"
+                label="Tìm kiếm phòng"
+                style={{ paddingBottom: "2vh" }}
+                onChange={(e, newVal) => {
+                  setSelectedRoom(() => newVal);
+                  setState({ ...state, roomId: newVal.id });
+                }}
+                renderInput={(params) => (
+                  <TextField
+                    {...params}
+                    label="Tìm kiếm phòng"
+                    variant="standard"
+                  />
+                )}
+              />
+            )}
+            <FormGroup>
+              <Label for="dateBegin">Ngày bắt đầu ở</Label>
+              <Input
+                id="dateBegin"
+                name="dateBegin"
+                type="date"
+                onChange={(event) => handleOnchangeInput(event, "dateBegin")}
+                value={state.dateBegin}
+              />
+            </FormGroup>
+            <FormGroup>
+              <Label for="numberOfMonth">Số tháng dự kiến lưu trú</Label>
+              <Input
+                id="numberOfMonth"
+                name="numberOfMonth"
+                onChange={(event) =>
+                  handleOnchangeInput(event, "numberOfMonth")
+                }
+                value={state.numberOfMonth}
+              />
+            </FormGroup>
+            {/* <Label for="gender">Tình trạng hóa đơn</Label>
+            <FormGroup>
+              <select
+                onChange={(event) =>
+                  handleOnchangeInput(event, "domitoryFeeStatus")
+                }
+              >
+                <option>--Chọn tình trạng hóa đơn--</option>
+                <option value={true}>Đã thanh toán</option>
+                <option value={false}>Chưa thanh toán</option>
+              </select>
+            </FormGroup> */}
+            <FormGroup>
+              <Label for="gender">Tình trạng hóa đơn</Label>
+              <FormControl style={{ width: "100%", maxWidth: "unset" }}>
+                <Select
+                  name={"domitoryFeeStatus"}
+                  id="domitoryFeeStatus"
+                  value={state.domitoryFeeStatus}
+                  style={{ width: "100%", maxWidth: "unset" }}
+                  onChange={(event) =>
+                    handleOnchangeInput(event, "domitoryFeeStatus")
+                  }
+                >
+                  <MenuItem value={false}>Chưa thanh toán</MenuItem>
+                  <MenuItem value={true}>Đã thanh toán</MenuItem>
+                </Select>
+              </FormControl>
+            </FormGroup>
+            <FormGroup>
+              <FormControl style={{ width: "100%", maxWidth: "unset" }}>
+                <Label>Tình trạng đăng ký</Label>
+                <Select
+                  name={"status"}
+                  id="status"
+                  value={state.status}
+                  style={{ width: "100%", maxWidth: "unset" }}
+                  onChange={(event) => handleOnchangeInput(event, "status")}
+                >
+                  <MenuItem value={true}>Còn hạn</MenuItem>
+                  <MenuItem value={false}>Hết hạn</MenuItem>
+                </Select>
+              </FormControl>
+            </FormGroup>
+            <div className="modalConfirmationControlButtons">
+              <Button color="secondary" onClick={props.toggle}>
+                Quay lại
+              </Button>
+              <Button color="primary" onClick={() => handleUpdateRegister()}>
+                Cập nhật
+              </Button>
+            </div>
+          </Form>
+        </ModalBody>
+        {/* Nút tắt phần modal */}
+        {/* <ModalFooter>
                     <Button color="primary" onClick={props.toggle}>
                         Create
                     </Button>{' '}
@@ -148,9 +198,9 @@ function UpdateRegister(props) {
                         Cancel
                     </Button>
                 </ModalFooter> */}
-            </Modal>
-        </div>
-    );
+      </Modal>
+    </div>
+  );
 }
 
 export default UpdateRegister;
